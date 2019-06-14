f795f9c09a52624642ff934b589630da28bc740f
Soorya <46225036+SooryaVN@users.noreply.github.com>
2019-06-14 21:59:29 +0530

    Update prime number given range.py

--- prime number given range.py
+++ prime number given range.py
@@ -1,9 +1,10 @@
-a,b=map(int,input().split())
-for i in range(a+1,b):
-    for j in range(2,i):
-        if(i%j==0):
-            break
-    else:
-        print(i,end=" ")
-    
-        
+I,R=map(int,input().split())
+m=list() 
+for i in range(I,R+1):
+  count=0
+  for j in range(1,i+1):
+      if(i%j==0):
+         count+=1
+  if(count==2):
+        m.append(i) 
+print(len(m))
